569ceced080b72770888d78312cb694626861dd7
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-19 17:45:28 -0700

    Add event listeners, keydown and click events, event handler functions, and conditional statements to script.js so that when 1, 2 and 3 are pressed on the keyboard, the corresponding numbered box on the page will display a specific emoji and the clear button can be clicked to clear the boxes of emojis and return them back to their original numbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
 
+document.addEventListener("keydown", function (e) {
+  // target the value of the key
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "🌕";
+  } else if (key === "2") {
+    box2.innerText = "🌈";
+  } else if (key === "3") {
+    box3.innerText = "💫";
+  }
+});
+
+// add an event that will clear the emojis in the three boxes
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
